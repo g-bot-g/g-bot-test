b5be7c8435e37fe484f85eeaa93de21d648742ce
Gerry Gao <gao88915@gmail.com>
2024-09-24 21:11:04 +1000

    PRND-2 - Development
    PRND-2 - Development

---
+++ gitbot/PRND2.swift
@@ -0,0 +1,24 @@
+//
+//  PRND2.swift
+//  gitbot
+//
+//  Created by Gerry Gao on 24/9/2024.
+//
+
+import Foundation
+
+class PRND2 {
+    var data: String
+    
+    var changeList: [String] = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+    ]
+    
+    private init() {
+        self.data = "data"
+    }
+}
