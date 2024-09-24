183c6440e0e43fbe69fc67bbec74e106e9c7023b
g-bot-g <gerry88915@gmail.com>
2024-09-24 20:26:28 +1000

    Add files via upload

---
+++ gitbot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gitbot
+//
+//  Created by Gerry Gao on 24/9/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gitbot/gitbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitbotApp.swift
+//  gitbot
+//
+//  Created by Gerry Gao on 24/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct gitbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
