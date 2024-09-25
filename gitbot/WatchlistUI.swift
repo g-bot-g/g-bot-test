2a5d0a55a196491770b387360149f52edcd09ba9
Gerry915 <gao88915@gmail.com>
2024-09-25 21:41:25 +1000

    add watchlist UI
    add section header
    
    add list body
    
    add stock item

---
+++ gitbot/WatchlistUI.swift
@@ -0,0 +1,17 @@
+//
+//  WatchlistUI.swift
+//  gitbot
+//
+//  Created by Gerry Gao on 25/9/2024.
+//
+
+import Foundation
+
+class WatchlistUI {
+    var data: [String] = [
+        "Navigation",
+        "Section header",
+        "List body",
+        "Stock items"
+    ]
+}
