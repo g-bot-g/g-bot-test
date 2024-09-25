866fbc7f90e408e2156916f7e36f9b69c44796db
Gerry915 <gao88915@gmail.com>
2024-09-25 19:58:08 +1000

    add prnd-1
    prnd1

--- gitbot/RemoteManager.swift
+++ gitbot/RemoteManager.swift
@@ -1,7 +1,9 @@
 class RemoteManager {
+    static let shared = RemoteManager()
+    var data = "test1"
+    
+    var prnd1 = "prnd1"
+    var prnd1more = "prnd1more"
 
-  static let shared = RemoteManager()
-  var data = "test1"
-  
-  private init() {}
+    private init() {}
 }
